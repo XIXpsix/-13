7d3917cc653e87559bf41b3f6a9ddd374fa61d7b
XIXpsix <lebedevdima902@gmail.com>
2024-11-26 13:35:37 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Input (string[] args)
+        {
+            Console.Write(mesage);
+            return int.Parse(Console.ReadLine());
+        }
+        static int Num(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+        public static void Borput(int result)
+        {
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[]args)
+        {
+            int x = Input("x=");
+            int y = Input("y=");
+            int result = Num(x,y);
+            Borput(result);
+            Console.Read();
+        }
+    }
+}
